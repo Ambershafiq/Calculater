c1250231294a5ef611ac628d24d7ba5e1ffa2624
Ambershafiq <49882193+Ambershafiq@users.noreply.github.com>
2019-11-29 12:09:23 +0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
-// write code here
+$ (document).ready (function(e){
+    $ ('.btn').click( function(e){
+        $('#result').val($('#result').val()+ $(this).val());
+
+});
+
+
+$('#clear').click (function(e){
+ $('#result').val('');    
+});
+$('#calculate').click (function(e){
+$('#result').val(eval($('#result').val()));
+
+ });
+
+});
